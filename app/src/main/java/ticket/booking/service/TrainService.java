64c6a268d61c92e04c9876a4bddab90a686719ef
Vaibhav Verma <vermavaibhav561@gmail.com>
2025-04-08 10:29:01 +0530

    Initial commit

---
+++ app/src/main/java/ticket/booking/service/TrainService.java
@@ -0,0 +1,79 @@
+package ticket.booking.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ticket.booking.entities.Train;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TrainService {
+
+    private List<Train> trainList;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final String TRAIN_DB_PATH = Paths.get("src", "main", "java", "ticket", "booking", "localDB", "trains.json").toAbsolutePath().toString();
+
+
+
+
+    public TrainService() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(TRAIN_DB_PATH);
+        if (inputStream == null) {
+            throw new FileNotFoundException("Could not find trains.json in resources.");
+        }
+        trainList = objectMapper.readValue(inputStream, new TypeReference<List<Train>>() {});
+    }
+
+    public List<Train> searchTrains(String source, String destination) {
+        return trainList.stream()
+                .filter(train -> validTrain(train, source, destination))
+                .collect(Collectors.toList());
+    }
+
+    public void addTrain(Train newTrain) {
+        Optional<Train> existingTrain = trainList.stream()
+                .filter(train -> train.getTrainId().equalsIgnoreCase(newTrain.getTrainId()))
+                .findFirst();
+
+        if (existingTrain.isPresent()) {
+            updateTrain(newTrain);
+        } else {
+            trainList.add(newTrain);
+            saveTrainListToFile();
+        }
+    }
+
+    public void updateTrain(Train updatedTrain) {
+        OptionalInt index = IntStream.range(0, trainList.size())
+                .filter(i -> trainList.get(i).getTrainId().equalsIgnoreCase(updatedTrain.getTrainId()))
+                .findFirst();
+
+        if (index.isPresent()) {
+            trainList.set(index.getAsInt(), updatedTrain);
+            saveTrainListToFile();
+        } else {
+            addTrain(updatedTrain);
+        }
+    }
+
+    private void saveTrainListToFile() {
+        try {
+            // Save to real file path (important, since you can't write to resource inside the JAR)
+            String path = "app/src/main/resources/" + TRAIN_DB_PATH;
+            objectMapper.writeValue(new File(path), trainList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean validTrain(Train train, String source, String destination) {
+        List<String> stationOrder = train.getStations();
+        int sourceIndex = stationOrder.indexOf(source.toLowerCase());
+        int destinationIndex = stationOrder.indexOf(destination.toLowerCase());
+        return sourceIndex != -1 && destinationIndex != -1 && sourceIndex < destinationIndex;
+    }
+}
